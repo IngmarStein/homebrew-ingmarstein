3f5b92ddefa9f86533da814911513cfc5ada865a
Ingmar Stein <490610+IngmarStein@users.noreply.github.com>
2026-03-26 09:27:37 +0100

    rtl-wmbus: fix component order

--- Formula/rtl-wmbus.rb
+++ Formula/rtl-wmbus.rb
@@ -5,12 +5,12 @@ class RtlWmbus < Formula
   sha256 "0da72c7f5b026a4c0d8dc9beabba021f147c950f52eb8e09f4e41884d4f32ebf"
   license "BSD-2-Clause"
 
+  head "https://github.com/weetmuts/rtl-wmbus.git", branch: "master"
+
   livecheck do
     url :stable
     strategy :github_latest
   end
-
-  head "https://github.com/weetmuts/rtl-wmbus.git", branch: "master"
 
   def install
     system "make", "release"
