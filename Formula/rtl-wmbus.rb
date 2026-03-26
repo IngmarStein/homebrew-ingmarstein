5dcbff41060525eae74b450b9ceb91a97ac8e582
Ingmar Stein <490610+IngmarStein@users.noreply.github.com>
2026-03-26 08:50:39 +0000

    rtl-wmbus: update 1.1.0 bottle.

--- Formula/rtl-wmbus.rb
+++ Formula/rtl-wmbus.rb
@@ -14,6 +14,9 @@ class RtlWmbus < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/ingmarstein/ingmarstein"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "f29a5a839c3b4e53ff1dc82820031bc4c2fb49863c184c1806bd6509fe523a5a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "795330eb2ad4bb9df0e90976c627539f7d9f8616c0445227189d6fba5caed468"
   end
 
   def install
